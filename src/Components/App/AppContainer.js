a28bcbd300de25890b6eddaa1403ecd74c969e63
Riyoung Choi <cheriey1214@gmail.com>
2018-05-29 22:37:57 +0900

    #6 Rendering thie Notifications

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -7,22 +7,23 @@ class AppContainer extends Component {
     // 스토어에 적용할 함수는 반드시 이 안에 있어야 함!
     super(props);
 
-    this._changeMessage = () => {
-      if(this.state.message === "Hello") {
-        this.setState({
-          message: "Bye bye"
-        })
-      } else {
-        this.setState({
-          message: "Hello"
-        })
-      }
-    }
-
     // 실질적인 store
     this.state = {
-      message: "Hello",
-      changeMessage: this._changeMessage,
+      notifications: {
+        "1": {
+          id: 1,
+          text: "something",
+          seen: false,
+        }, "2": {
+          id: 2,
+          text: "something else",
+          seen: false,
+        }, "3": {
+          id: 3,
+          text: "something else but different",
+          seen: false,
+        }
+      }
     }
   }
   render() {
